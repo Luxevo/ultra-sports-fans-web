5321e0dced7c40677a2514a424f25d3777fa5e27
JolanJ <jolan@luxevotech.com>
2026-03-18 17:26:26 -0400

    feat: add delete acount page

---
+++ app/delete-account/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function DeleteAccount() {
+  
+
+  return (
+    <p>To delete your account and data, open the Ultra Sports Fans app, go to Profile, and tap "Delete my account." Your account and all associated data will be permanently deleted.                
+        If you cannot access the app, email us at contact@ultrasportsfans.com to request account deletion.</p>
+  )
+    
+}
